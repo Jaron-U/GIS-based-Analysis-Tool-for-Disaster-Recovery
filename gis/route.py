44c40dd2e8c837fd974636d53d386bc64bddd687
Jianglong Yu <yujianglan98@gmail.com>
2023-02-08 13:55:01 -0800

    formatted functions and set the geojson style in the code instead of reading a file

--- gis/route.py
+++ gis/route.py
@@ -10,55 +10,60 @@ token = 'AAPKed0344fad75a47759b9659eae86034daRZO9WuLRC0mwoydQIcZg3E41PAlD6vFtpsU
 # api='https://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World/solve?'
 api="https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World/solve?<parameters>"
 
-# setup request parameters 
-
-
-#set the start point
-start = "-123.2737581,44.5739238"
-end = "-123.2788905,44.5669322"
-
-# Documentation on API parameters here: https://developers.arcgis.com/rest/network/api-reference/route-synchronous-service.htm#ESRI_SECTION2_C85EFA22DE5B48F3BE72E629EAB9049B
-parameters = {
-	# my house to OSU KEC
-	# "stops": "-123.2737581,44.5739238;-123.2788905,44.5669322",
-	"stops": start + ";" + end,
-	"token": token,
-	"f": "pjson",
-	"returnDirections": "true",
-	"directionsLanguage": "en"
+# set the geojson style
+templateDict = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": [
+            ]
+        },
+        "properties": {}
+    }]
 }
 
-# send API request
-response = requests.get(api, params=parameters)
+# setup request parameters 
+def getResponse(start, end):
+	# Documentation on API parameters here: https://developers.arcgis.com/rest/network/api-reference/route-synchronous-service.htm#ESRI_SECTION2_C85EFA22DE5B48F3BE72E629EAB9049B
+	parameters = {
+		# my house to OSU KEC
+		# "stops": "-123.2737581,44.5739238;-123.2788905,44.5669322",
+		"stops": start + ";" + end,
+		"token": token,
+		"f": "pjson",
+		"returnDirections": "true",
+		"directionsLanguage": "en"
+	}
+	# send API request
+	response = requests.get(api, params=parameters)
+	return response
 
-
-
-
-
-
-        # for feature in features:
-        #     properties = feature["properties"]
-        
-    #     dicts = json_data
-    # return dicts
- 
-# def write_json_data(dict):
-#     with open('D:/data/china_r.json','w') as r:
-#         json.dump(dict,r)
-
-# save results (note: to load into ArcGIS Pro requires some data-cleaning)
-if response.status_code == 200:
+def writeResponse(response, filename):
 	response_dict = json.loads(response.text)
+	# get the road data we need
 	new_coordinates = response_dict['routes']['features'][0]['geometry']['paths'][0]
-	dicts = {}
-	with open('directions.geojson','r',encoding='utf8') as f:
-		json_data = json.load(f)
-		geometry = json_data["features"][0]['geometry']
-		geometry['coordinates'] = new_coordinates
-	with open('d.geojson','w') as r:
-		json.dump(json_data,r)
+	geometry = templateDict["features"][0]['geometry']
+	# put the road data input the geojson style text
+	geometry['coordinates'] = new_coordinates
+	dict = templateDict
+	# write the formatted json into a file.
+	with open(filename,'w') as r:
+		json.dump(dict,r)
 
 
-else:
-	print("Error: ", response.text)
+
+if __name__ == '__main__':
+    #set the start point
+	start = "-122.2737581,44.5739238"
+	end = "-123.2788905,44.5669322"
+	response = getResponse(start, end)
+	if response.status_code == 200:
+    	# get the response file
+		with open('reponse.json','w') as f:
+			f.write(response.text)
+		writeResponse(response, 'directions.geojson')
+	else:
+		print("Error: ", response.text)
 
