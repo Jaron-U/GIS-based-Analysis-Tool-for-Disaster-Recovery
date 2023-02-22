d74487e43ebac477af0252b03d9d10a155b29420
Jianglong Yu <yujianglan98@gmail.com>
2023-02-21 18:34:28 -0800

    launch the html file

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # Flask server to serve as an API
 import data # our custom wrapper for the ArcGIS APIs
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "<p>Hello, World!</p>"
+	return render_template("temp.html")
 
 # TODO ADD ROUTE FOR GETTING DIRECTIONS FROM STOPS GIVEN GEOJSON PARAMETER
 @app.route("/route")
