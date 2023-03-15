e01fe78c716911f7b61b455017f0ade2015cfbd5
Jianglong Yu <yujianglan98@gmail.com>
2023-03-14 17:55:24 -0700

    minor update

--- static/js/webMap.js
+++ static/js/webMap.js
@@ -1,5 +1,8 @@
 'use strict'
 
+// use for receving the file content
+var jsonFeature
+
 document.addEventListener('DOMContentLoaded', bind)
 
 function bind() {
@@ -11,83 +14,81 @@ function bind() {
                 maxZoom: 19,
                 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
-        
-        //submit lister funtion 
-        document.getElementById('submit').addEventListener('click', () => {
-                        //get the service type
-                        var serviceType = document.getElementById('service').value
-                        console.log(serviceType)
-                        if (serviceType === "place"){
-                                getRoute(L, map)
-                        }
-            })
-
-        //visualize hazard data on the map when it's added
-        document.getElementById("uploadFile").addEventListener("change", function() {
-            var fReader = new FileReader();
-            var file = document.getElementById("uploadFile").files[0]
-            fReader.readAsText(file)
-            fReader.onload = function () {
-                var jsonFeature = JSON.parse(fReader.result)
-                L.geoJSON(jsonFeature).addTo(map)
-                //map.fitBounds(new L.featureGroup(jsonFeature.features[0].geometry.coordinates[0]).getBounds())
-                console.log(jsonFeature.features[0].geometry.coordinates[0])
-                return jsonFeature
-            }
-        });
 
         document.getElementById('p2dInfo').style.display = "none"
-        document.getElementById("service").value = "place"
         document.getElementById("service").addEventListener("change", function () {
-            if (document.getElementById('service').value == "place") {
-                document.getElementById('p2dInfo').style.display = "none"
-                document.getElementById('p2pInfo').style.display = ""
-            }
-            if (document.getElementById('service').value == "facility") {
-                document.getElementById('p2pInfo').style.display = "none"
-                document.getElementById('p2dInfo').style.display = ""
-            }
+                var serviceType = document.getElementById("service").value
+                if (serviceType == "place") {
+                        document.getElementById('p2dInfo').style.display = "none"
+                        document.getElementById('p2pInfo').style.display = ""
+                }
+                if (serviceType == "facility") {
+                        document.getElementById('p2pInfo').style.display = "none"
+                        document.getElementById('p2dInfo').style.display = ""
+                }
+        })
+
+        //visualize hazard data on the map when it's added
+        document.getElementById("uploadFile").addEventListener("change", function () {
+                var fReader = new FileReader();
+                var file = document.getElementById("uploadFile").files[0]
+                fReader.readAsText(file)
+                fReader.onload = function () {
+                        var jsonFeature = JSON.parse(fReader.result)
+                        L.geoJSON(jsonFeature).addTo(map)
+                        //map.fitBounds(new L.featureGroup(jsonFeature.features[0].geometry.coordinates[0]).getBounds())
+                        console.log(jsonFeature.features[0].geometry.coordinates[0])
+                }
+        });
+
+        //submit lister funtion 
+        document.getElementById('submit').addEventListener('click', () => {
+                //get the service type
+                var serviceType = document.getElementById('service').value
+                console.log(serviceType)
+                if (serviceType === "place") {
+                        getRoute(L, map)
+                }
         })
 }
 
 async function getRoute(L, map) {
-    //get location info, and convert them to float type
-    var oLat = parseFloat(document.getElementById('originLat').value);
-    var oLnt = parseFloat(document.getElementById('originLnt').value);
-    var dLat = parseFloat(document.getElementById('destinationLat').value);
-    var dLnt = parseFloat(document.getElementById('destinationLnt').value);
+        //get location info, and convert them to float type
+        var oLat = parseFloat(document.getElementById('originLat').value);
+        var oLnt = parseFloat(document.getElementById('originLnt').value);
+        var dLat = parseFloat(document.getElementById('destinationLat').value);
+        var dLnt = parseFloat(document.getElementById('destinationLnt').value);
 
-    //hard code for test
-    oLnt = -123.27898217097302
-    oLat = 44.57272617278602
-    dLnt = -123.26839325796291
-    dLat = 44.56848686030949
-
-    var fReader = new FileReader(); //start loading/reading the uploaded file
-    fReader.readAsText(document.getElementById('uploadFile').files[0]);
+        //hard code for test
+        oLnt = -123.27898217097302
+        oLat = 44.57272617278602
+        dLnt = -123.26839325796291
+        dLat = 44.56848686030949
         
-    fReader.onload = function() { //function() will run upon completing the load of the uploaded file
         //convert the input data into json type
         const data = {
-            stops: [[oLnt, oLat], [dLnt, dLat]],
-            hazards: JSON.parse(fReader.result)
+                stops: [
+                        [oLnt, oLat],
+                        [dLnt, dLat]
+                ],
+                hazards: "JSON.parse(fReader.result)"
         };
         console.log(data)
         //fetch the data from backend
         fetch('/route/submit-data', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                        'Content-Type': 'application/json'
+                }
         })
         .then(response => response.json())
         .then(data => {
-            console.log('Success:', data);
-            L.geoJSON(data).addTo(map);
+                console.log('Success:', data);
+                L.geoJSON(data).addTo(map);
         })
         .catch((error) => {
-            console.error('Error:', error);
+                console.error('Error:', error);
         });
-    }
+
 }
